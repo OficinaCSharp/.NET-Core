70835fb167d84a48b2d1254c7dce4f487a30bb65
Ron <ronaldosg1999@outlook.com>
2019-03-19 15:33:17 -0300

    Arrumando o projeto da aula2

--- Aula2/Aula2/Program.cs
+++ Aula2/Aula2/Program.cs
@@ -6,31 +6,52 @@ namespace Aula2
     {
         static void Main(string[] args)
         {
-            double n1, n2, n3, n4;
+            String nome;
+            int idade;
+            double saldo;
 
-            Console.WriteLine("Digita sua nota");
-            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
 
-            Console.WriteLine("Digita sua nota");
-            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digita sua nota");
-            n3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite seu saldo: ");
+            saldo = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Digita sua nota");
-            n4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Meu nome e {nome}, tenho {idade} anos, meu saldo na conta é de {saldo.ToString("c")} ");
 
-            double media = (n1 + n2 + n3 + n4) / 4;
+            Console.WriteLine("-----------------------------------------------------------------");
 
-            if(media >= 7)
+            double valor1 = 50, valor2 = 35;
+
+            double soma = valor1 + valor2; //Somar
+            double sub = valor1 - valor2; //Subtrair
+            double mult = valor1 * valor2; //Multiplicar
+            double div = valor1 / valor2; //Dividir
+            double restoDiv = valor1 % valor2; //Resto da Divisão
+
+            Console.WriteLine($"Resultado da soma é: {soma} ");
+            Console.WriteLine($"Resultado da subtração é: {sub} ");
+            Console.WriteLine($"Resultado da multiplicação é: {mult} ");
+            Console.WriteLine($"Resultado da divisão é: {div} ");
+            Console.WriteLine($"Resultado da resto da divisão é: {restoDiv} ");
+
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            String chuva;
+
+            Console.WriteLine("Hoje está chovendo? (s) ou (n)");
+            chuva = Console.ReadLine();
+
+            if(chuva == "s")
             {
-                Console.WriteLine("Aluno aprovado");
+                Console.WriteLine("Não irei viajar");
             }
             else
             {
-                Console.WriteLine("Aluno reprovado");
+                Console.WriteLine("Eu irei viajar");
             }
-
         }
     }
 }
